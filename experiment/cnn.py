b0862f93e890f83f68a398b7fe4a015939669d25
briannoogin <brian.qh.nguyen@gmail.com>
2019-07-07 21:31:32 -0500

    added skip hyperconnections to cnn

--- experiment/cnn.py
+++ experiment/cnn.py
@@ -57,6 +57,8 @@ from __future__ import division
 import os
 import warnings
 
+import keras.backend as K
+from keras.backend import zeros
 from keras_applications.imagenet_utils import _obtain_input_shape, get_submodules_from_kwargs
 from keras_applications import imagenet_utils
 import keras 
@@ -214,9 +216,10 @@ def baseline_ANRL_MobileNet(input_shape=None,
             img_input = input_tensor
   
     # changed the strides from 2 to 1 since cifar-10 images are smaller
+    # IoT Node
+    iot = _conv_block(img_input, 32, alpha, strides=(1, 1))
     # edge 
-    edge = _conv_block(img_input, 32, alpha, strides=(1, 1))
-    edge = _depthwise_conv_block(edge, 64, alpha, depth_multiplier, block_id=1)
+    edge = _depthwise_conv_block(iot, 64, alpha, depth_multiplier, block_id=1)
 
     edge = _depthwise_conv_block(edge, 128, alpha, depth_multiplier,
                               strides=(1, 1), block_id=2)
@@ -248,6 +251,233 @@ def baseline_ANRL_MobileNet(input_shape=None,
 
         cloud = layers.GlobalAveragePooling2D()(cloud)
         cloud = layers.Reshape(shape, name='reshape_1')(cloud)
+        # dropout is only in the cloud node, we can potentially use it
+        cloud = layers.Dropout(dropout, name='dropout')(cloud)
+        cloud = layers.Conv2D(classes, (1, 1),
+                          padding='same',
+                          name='conv_preds')(cloud)
+        cloud = layers.Reshape((classes,), name='reshape_2')(cloud)
+        cloud = layers.Activation('softmax', name='act_softmax')(cloud)
+    else:
+        if pooling == 'avg':
+            cloud = layers.GlobalAveragePooling2D()(cloud)
+        elif pooling == 'max':
+            cloud = layers.GlobalMaxPooling2D()(cloud)
+
+    # Ensure that the model takes into account
+    # any potential predecessors of `input_tensor`.
+    if input_tensor is not None:
+        inputs = keras_utils.get_source_inputs(input_tensor)
+    else:
+        inputs = img_input
+
+    # Create model.
+    model = keras.Model(inputs, cloud, name='mobilenet_%0.2f_%s' % (alpha, rows))
+
+    # Load weights.
+    if weights == 'imagenet':
+        if alpha == 1.0:
+            alpha_text = '1_0'
+        elif alpha == 0.75:
+            alpha_text = '7_5'
+        elif alpha == 0.50:
+            alpha_text = '5_0'
+        else:
+            alpha_text = '2_5'
+
+        if include_top:
+            model_name = 'mobilenet_%s_%d_tf.h5' % (alpha_text, rows)
+            weight_path = BASE_WEIGHT_PATH + model_name
+            weights_path = keras_utils.get_file(model_name,
+                                                weight_path,
+                                                cache_subdir='models')
+        else:
+            model_name = 'mobilenet_%s_%d_tf_no_top.h5' % (alpha_text, rows)
+            weight_path = BASE_WEIGHT_PATH + model_name
+            weights_path = keras_utils.get_file(model_name,
+                                                weight_path,
+                                                cache_subdir='models')
+        model.load_weights(weights_path)
+    elif weights is not None:
+        model.load_weights(weights)
+
+    return model
+
+def skipconnections_ANRL_MobileNet(input_shape=None,
+              alpha=1.0,
+              depth_multiplier=1,
+              dropout=1e-3,
+              include_top=True,
+              weights='imagenet',
+              input_tensor=None,
+              pooling=None,
+              classes=1000,
+              **kwargs):
+    """Instantiates the MobileNet architecture.
+
+    # Arguments
+        input_shape: optional shape tuple, only to be specified
+            if `include_top` is False (otherwise the input shape
+            has to be `(224, 224, 3)`
+            (with `channels_last` data format)
+            or (3, 224, 224) (with `channels_first` data format).
+            It should have exactly 3 inputs channels,
+            and width and height should be no smaller than 32.
+            E.g. `(200, 200, 3)` would be one valid value.
+        alpha: controls the width of the network. This is known as the
+            width multiplier in the MobileNet paper.
+            - If `alpha` < 1.0, proportionally decreases the number
+                of filters in each layer.
+            - If `alpha` > 1.0, proportionally increases the number
+                of filters in each layer.
+            - If `alpha` = 1, default number of filters from the paper
+                 are used at each layer.
+        depth_multiplier: depth multiplier for depthwise convolution. This
+            is called the resolution multiplier in the MobileNet paper.
+        dropout: dropout rate
+        include_top: whether to include the fully-connected
+            layer at the top of the network.
+        weights: one of `None` (random initialization),
+              'imagenet' (pre-training on ImageNet),
+              or the path to the weights file to be loaded.
+        input_tensor: optional Keras tensor (i.e. output of
+            `layers.Input()`)
+            to use as image input for the model.
+        pooling: Optional pooling mode for feature extraction
+            when `include_top` is `False`.
+            - `None` means that the output of the model
+                will be the 4D tensor output of the
+                last convolutional block.
+            - `avg` means that global average pooling
+                will be applied to the output of the
+                last convolutional block, and thus
+                the output of the model will be a
+                2D tensor.
+            - `max` means that global max pooling will
+                be applied.
+        classes: optional number of classes to classify images
+            into, only to be specified if `include_top` is True, and
+            if no `weights` argument is specified.
+
+    # Returns
+        A Keras model instance.
+
+    # Raises
+        ValueError: in case of invalid argument for `weights`,
+            or invalid input shape.
+        RuntimeError: If attempting to run this model with a
+            backend that does not support separable convolutions.
+    """
+    global backend, layers, models, keras_utils
+    backend,layers,models, keras_utils = get_submodules_from_kwargs(kwargs)
+    if not (weights in {'imagenet', None} or os.path.exists(weights)):
+        raise ValueError('The `weights` argument should be either '
+                         '`None` (random initialization), `imagenet` '
+                         '(pre-training on ImageNet), '
+                         'or the path to the weights file to be loaded.')
+
+    if weights == 'imagenet' and include_top and classes != 1000:
+        raise ValueError('If using `weights` as `"imagenet"` with `include_top` '
+                         'as true, `classes` should be 1000')
+    backend = keras.backend
+    layers = keras.layers
+    # Determine proper input shape and default size.
+    if input_shape is None:
+        default_size = 224
+    else:
+        if backend.image_data_format() == 'channels_first':
+            rows = input_shape[1]
+            cols = input_shape[2]
+        else:
+            rows = input_shape[0]
+            cols = input_shape[1]
+
+        if rows == cols and rows in [128, 160, 192, 224]:
+            default_size = rows
+        else:
+            default_size = 224
+
+    input_shape = _obtain_input_shape(input_shape,
+                                      default_size=default_size,
+                                      min_size=32,
+                                      data_format=backend.image_data_format(),
+                                      require_flatten=include_top,
+                                      weights=weights)
+
+    if backend.image_data_format() == 'channels_last':
+        row_axis, col_axis = (0, 1)
+    else:
+        row_axis, col_axis = (1, 2)
+    rows = input_shape[row_axis]
+    cols = input_shape[col_axis]
+
+    if weights == 'imagenet':
+        if depth_multiplier != 1:
+            raise ValueError('If imagenet weights are being loaded, '
+                             'depth multiplier must be 1')
+
+        if alpha not in [0.25, 0.50, 0.75, 1.0]:
+            raise ValueError('If imagenet weights are being loaded, '
+                             'alpha can be one of'
+                             '`0.25`, `0.50`, `0.75` or `1.0` only.')
+
+        if rows != cols or rows not in [128, 160, 192, 224]:
+            rows = 224
+            warnings.warn('`input_shape` is undefined or non-square, '
+                          'or `rows` is not in [128, 160, 192, 224]. '
+                          'Weights for input shape (224, 224) will be'
+                          ' loaded as the default.')
+
+    if input_tensor is None:
+        img_input = layers.Input(shape=input_shape)
+    else:
+        if not backend.is_keras_tensor(input_tensor):
+            img_input = layers.Input(tensor=input_tensor, shape=input_shape)
+        else:
+            img_input = input_tensor
+    # changed the strides from 2 to 1 since cifar-10 images are smaller
+    # IoT node
+    iot = _conv_block(img_input, 32, alpha, strides=(1, 1))
+    # edge 
+    edge = _depthwise_conv_block(iot, 64, alpha, depth_multiplier, block_id=1)
+
+    edge = _depthwise_conv_block(edge, 128, alpha, depth_multiplier,
+                              strides=(1, 1), block_id=2)
+    connection_edgefog = _depthwise_conv_block(edge, 128, alpha, depth_multiplier, block_id=3) # size:  (None, 31, 31, 64) 
+    # skip hyperconnection, used 1x1 convolution to project shape of node output into (7,7,256)
+    connection_edgecloud = layers.Conv2D(256,(1,1),strides = 4, use_bias = False, name = "skip_hyperconnection_edgecloud")(connection_edgefog)
+
+    # fog node
+    fog = _depthwise_conv_block(connection_edgefog, 256, alpha, depth_multiplier, # size: (None, 32, 32, 64)
+                              strides=(2, 2), block_id=4)
+    fog = _depthwise_conv_block(fog, 256, alpha, depth_multiplier, block_id=5)
+
+    fog = _depthwise_conv_block(fog, 512, alpha, depth_multiplier,
+                              strides=(2, 2), block_id=6)
+    fog = _depthwise_conv_block(fog, 512, alpha, depth_multiplier, block_id=7)
+    fog = _depthwise_conv_block(fog, 512, alpha, depth_multiplier, block_id=8) #size : (None, 7, 7, 256) 
+    # pad from (7,7,256) to (8,8,256)
+    connection_fogcloud = layers.ZeroPadding2D(padding = ((0, 1), (0, 1)), name = "cloud_connection_padding")(fog)
+    connection_cloud = layers.add([connection_fogcloud,connection_edgecloud], name = "connection_cloud")
+
+    # cloud node
+    cloud = _depthwise_conv_block(connection_cloud, 512, alpha, depth_multiplier, block_id=9) # size: (None, 7, 7, 256)
+    cloud = _depthwise_conv_block(cloud, 512, alpha, depth_multiplier, block_id=10)
+    cloud = _depthwise_conv_block(cloud, 512, alpha, depth_multiplier, block_id=11)
+
+    cloud = _depthwise_conv_block(cloud, 1024, alpha, depth_multiplier,
+                              strides=(2, 2), block_id=12)
+    cloud = _depthwise_conv_block(cloud, 1024, alpha, depth_multiplier, block_id=13)
+
+    if include_top:
+        if backend.image_data_format() == 'channels_first':
+            shape = (int(1024 * alpha), 1, 1)
+        else:
+            shape = (1, 1, int(1024 * alpha))
+
+        cloud = layers.GlobalAveragePooling2D()(cloud)
+        cloud = layers.Reshape(shape, name='reshape_1')(cloud)
+        # dropout is only in the cloud node, we can potentially use it
         cloud = layers.Dropout(dropout, name='dropout')(cloud)
         cloud = layers.Conv2D(classes, (1, 1),
                           padding='same',
--- experiment/experiment3.py
+++ experiment/experiment3.py
@@ -5,9 +5,10 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Dense, BatchNormalization, Input, Dropout
 from keras.models import Model
 from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
 import math
 import os 
-from experiment.cnn import baseline_ANRL_MobileNet
+from experiment.cnn import baseline_ANRL_MobileNet, skipconnections_ANRL_MobileNet
 def view_model():
     model = MobileNet(weights = None,classes=10,input_shape = (32,32,3),dropout = 0)
     model.summary()
@@ -23,15 +24,16 @@ def main():
     height_shift_range=0.2,
     horizontal_flip=True,
     )
-    #model = MobileNet(weights = None,classes=10,input_shape = (32,32,3),dropout = 0, alpha = .5)
-    model = baseline_ANRL_MobileNet(weights = None,classes=10,input_shape = (32,32,3),dropout = 0, alpha = 1)
+    file_name = "GitHubANRL_cnn_skiphyperconnection_weights_alpha050_fixedstrides_dataaugmentation.h5"
+    checkpoint = ModelCheckpoint(file_name,verbose=1,save_best_only=True,save_weights_only = True)
+    #model = baseline_ANRL_MobileNet(weights = None,classes=10,input_shape = (32,32,3),dropout = 0, alpha = .5)
+    model = skipconnections_ANRL_MobileNet(weights = None,classes=10,input_shape = (32,32,3),dropout = 0, alpha = .5)
     model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     num_samples = len(x_train)
     batch_size = 128
     steps_per_epoch = math.ceil(num_samples / batch_size)
-    model.fit_generator(datagen.flow(x_train,y_train,batch_size = batch_size),epochs = 75,validation_data = (x_test,y_test), steps_per_epoch = steps_per_epoch, verbose = 2)
+    model.fit_generator(datagen.flow(x_train,y_train,batch_size = batch_size),epochs = 75,validation_data = (x_test,y_test), steps_per_epoch = steps_per_epoch, verbose = 2,callbacks = [checkpoint])
     print(model.evaluate(x_test,y_test))
-    file_name = "GitHubANRL_cnn_weights_alpha100_fixedstrides_dataaugmentation.h5"
     model.save_weights(file_name)
     os.system('gsutil -m -q cp -r %s gs://anrl-storage/models' % file_name)
 # cnn experiment 
