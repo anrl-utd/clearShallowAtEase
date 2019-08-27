1fc94d931442870a1cc91ed18a7f12a0052836ed
Ashkan Y <yousefpour.ashkan@gmail.com>
2019-08-27 11:46:13 -0700

    update gitignore and some changes to deepFogGuardCamera

--- KerasSingleLaneExperiment/mlp_deepFogGuard_camera.py
+++ KerasSingleLaneExperiment/mlp_deepFogGuard_camera.py
@@ -7,8 +7,8 @@ import random
 def define_deepFogGuard_MLP(num_vars,
                             num_classes,
                             hidden_units,
-                            survivability_setting = [1.0,1.0,1.0], # survivability of a node between 0 and 1, 
-                            skip_hyperconnection_config = [1,1,1], # binary representating if a skip hyperconnection is alive
+                            survivability_setting = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0], # survivability of a node between 0 and 1, [f1,f2,f3,f4,e1,e2,e3,e4]
+                            skip_hyperconnection_config = [1,1,1,1,1,1,1], # binary representating if a skip hyperconnection is alive. source of skip hyperconnections: [e1,e2,e3,e4,f3,f4,f2]
                             hyperconnection_weights_scheme = 1):
     """Define a deepFogGuard model.
     ### Naming Convention
@@ -18,28 +18,20 @@ def define_deepFogGuard_MLP(num_vars,
         num_classes (int): specifies number of classes to be outputted by the model
         hidden_units (int): specifies number of hidden units per layer in network
         survivability_setting (list): specifies the survival rate of each node in the network
-        skip_hyperconnection_config (list): specifies the alive skip hyperconnections in the network, default value is [1,1,1]
-        hyperconnection_weights (list): specifies the probability, default value is [1,1,1]
+        skip_hyperconnection_config (list): specifies the alive skip hyperconnections in the network, default value is [1,1,1,1,1,1,1]
         hyperconnection_weights_scheme (int): determines if the hyperconnections should be based on surivive_rates, 1: weighted 1, 2: weighted by weighted survival of multiple nodes, 3: weighted by survival of single node only, 4: weights are randomly weighted from 0-1, 5: weights are randomly weighted from 0-10
     ### Returns
         Keras Model object
     """
 
-    hyperconnection_weight_IoTf2,hyperconnection_weight_ef2,hyperconnection_weight_ef1,hyperconnection_weight_f2f1, hyperconnection_weight_f2c, hyperconnection_weight_f1c = set_hyperconnection_weights(
-        hyperconnection_weights_scheme, 
-        survivability_setting, 
-        skip_hyperconnection_config)
-    multiply_hyperconnection_weight_layer_IoTf2, multiply_hyperconnection_weight_layer_ef2, multiply_hyperconnection_weight_layer_ef1, multiply_hyperconnection_weight_layer_f2f1, multiply_hyperconnection_weight_layer_f2c, multiply_hyperconnection_weight_layer_f1c = define_hyperconnection_weight_lambda_layers(
-        hyperconnection_weight_IoTf2, 
-        hyperconnection_weight_ef2, 
-        hyperconnection_weight_ef1, 
-        hyperconnection_weight_f2f1, 
-        hyperconnection_weight_f2c, 
-        hyperconnection_weight_f1c)
+    hyperconnection_weight = {} # define the hyperconnection_weight as dictionary
+    connection_ends = ["e1f2","e2f2","e3f2","e4f2","f3f1","f4f1","f2c","e1f3","e2f4","e3f4","e4f4","f3f2","f4f2","f2f1","f1c"]
+
+    hyperconnection_weight = set_hyperconnection_weights(hyperconnection_weights_scheme, survivability_setting, skip_hyperconnection_config, connection_ends)
+    multiply_hyperconnection_weight_layer = define_hyperconnection_weight_lambda_layers(hyperconnection_weight, connection_ends)
    
     # IoT node
     img_input = Input(shape = (num_vars,))
-    iot_output = define_MLP_deepFogGuard_architecture_IoT(img_input, hidden_units)
 
     # edge node
     edge_output = define_MLP_deepFogGuard_architecture_edge(iot_output, hidden_units)
@@ -58,92 +50,97 @@ def define_deepFogGuard_MLP(num_vars,
     return model
 
 
-def set_hyperconnection_weights(hyperconnection_weights_scheme, survivability_setting, skip_hyperconnection_config):
+def set_hyperconnection_weights(hyperconnection_weights_scheme, survivability_setting, skip_hyperconnection_config, connection_ends):
     # weighted by 1
     if hyperconnection_weights_scheme == 1: 
-        hyperconnection_weight_IoTf2  = 1
-        hyperconnection_weight_ef2 = 1
-        hyperconnection_weight_ef1 = 1
-        hyperconnection_weight_f2f1 = 1
-        hyperconnection_weight_f2c = 1
-        hyperconnection_weight_f1c = 1
+        for connection_end in connection_ends:
+            hyperconnection_weight[connection_end] = 1
     # normalized survivability
     elif hyperconnection_weights_scheme == 2:
-        hyperconnection_weight_IoTf2  = 1 / (1+survivability_setting[0])
-        hyperconnection_weight_ef2 = survivability_setting[0] / (1 + survivability_setting[0])
-        hyperconnection_weight_ef1 = survivability_setting[0] / (survivability_setting[0] + survivability_setting[1])
-        hyperconnection_weight_f2f1 = survivability_setting[1] / (survivability_setting[0] + survivability_setting[1])
-        hyperconnection_weight_f2c = survivability_setting[1] / (survivability_setting[1] + survivability_setting[2])
-        hyperconnection_weight_f1c = survivability_setting[2] / (survivability_setting[1] + survivability_setting[2])
+        hyperconnection_weight["e1f2"] = survivability_setting[]
+        hyperconnection_weight["e2f2"] = 
+        hyperconnection_weight["e3f2"] = 
+        hyperconnection_weight["e4f2"] = 
+        hyperconnection_weight["f3f1"] = 
+        hyperconnection_weight["f4f1"] = 
+        hyperconnection_weight["f2c"] = 
+        hyperconnection_weight["e1f3"] = 
+        hyperconnection_weight["e2f4"] = 
+        hyperconnection_weight["e3f4"] = 
+        hyperconnection_weight["e4f4"] = 
+        hyperconnection_weight["f3f2"] = 
+        hyperconnection_weight["f4f2"] = 
+        hyperconnection_weight["f2f1"] = 
+        hyperconnection_weight["f1c"] = 
     # survivability
     elif hyperconnection_weights_scheme == 3:
-        hyperconnection_weight_IoTf2  = 1
-        hyperconnection_weight_ef2 = survivability_setting[0] 
-        hyperconnection_weight_ef1 = survivability_setting[0] 
-        hyperconnection_weight_f2f1 = survivability_setting[1]
-        hyperconnection_weight_f2c = survivability_setting[1] 
-        hyperconnection_weight_f1c = survivability_setting[2]
+        connection_ends = ["e1f2","e2f2","e3f2","e4f2","f3f1","f4f1","f2c","e1f3","e2f4","e3f4","e4f4","f3f2","f4f2","f2f1","f1c"]
+        hyperconnection_weight["e1f2"] = survivability_setting[4]
+        hyperconnection_weight["e2f2"] = survivability_setting[5]
+        hyperconnection_weight["e3f2"] = survivability_setting[6]
+        hyperconnection_weight["e4f2"] = survivability_setting[7]
+        hyperconnection_weight["f3f1"] = survivability_setting[2]
+        hyperconnection_weight["f4f1"] = survivability_setting[3]
+        hyperconnection_weight["f2c"] = survivability_setting[1]
+        hyperconnection_weight["e1f3"] = survivability_setting[4]
+        hyperconnection_weight["e2f4"] = survivability_setting[5]
+        hyperconnection_weight["e3f4"] = survivability_setting[6]
+        hyperconnection_weight["e4f4"] = survivability_setting[7]
+        hyperconnection_weight["f3f2"] = survivability_setting[2]
+        hyperconnection_weight["f4f2"] = survivability_setting[3]
+        hyperconnection_weight["f2f1"] = survivability_setting[1]
+        hyperconnection_weight["f1c"] = survivability_setting[0]
+
     # randomly weighted between 0 and 1
     elif hyperconnection_weights_scheme == 4:
-        hyperconnection_weight_IoTf2  = random.uniform(0,1)
-        hyperconnection_weight_ef2 = random.uniform(0,1)
-        hyperconnection_weight_ef1 = random.uniform(0,1)
-        hyperconnection_weight_f2f1 = random.uniform(0,1)
-        hyperconnection_weight_f2c = random.uniform(0,1)
-        hyperconnection_weight_f1c = random.uniform(0,1)
+        for connection_end in connection_ends:
+            hyperconnection_weight[connection_end] = random.uniform(0,1)
     # randomly weighted between 0 and 10
     elif hyperconnection_weights_scheme == 5:
-        hyperconnection_weight_IoTf2  = random.uniform(0,10)
-        hyperconnection_weight_ef2 = random.uniform(0,10)
-        hyperconnection_weight_ef1 = random.uniform(0,10)
-        hyperconnection_weight_f2f1 = random.uniform(0,10)
-        hyperconnection_weight_f2c = random.uniform(0,10)
-        hyperconnection_weight_f1c = random.uniform(0,10)
+        for connection_end in connection_ends:
+            hyperconnection_weight[connection_end] = random.uniform(0,10)
     # randomly weighted by .5
     elif hyperconnection_weights_scheme == 6:
-        hyperconnection_weight_IoTf2  = .5
-        hyperconnection_weight_ef2 = .5
-        hyperconnection_weight_ef1 = .5
-        hyperconnection_weight_f2f1 = .5
-        hyperconnection_weight_f2c = .5
-        hyperconnection_weight_f1c = .5
+        for connection_end in connection_ends:
+            hyperconnection_weight[connection_end] = 0.5
     else:
         raise ValueError("Incorrect scheme value")
-    hyperconnection_weight_IoTf2, hyperconnection_weight_ef1, hyperconnection_weight_f2c = remove_skip_hyperconnection_for_sensitvity_experiment(
-        skip_hyperconnection_config, 
-        hyperconnection_weight_IoTf2, 
-        hyperconnection_weight_ef1, 
-        hyperconnection_weight_f2c)
-    return (hyperconnection_weight_IoTf2,hyperconnection_weight_ef2,hyperconnection_weight_ef1,hyperconnection_weight_f2f1, hyperconnection_weight_f2c, hyperconnection_weight_f1c)
+    hyperconnection_weight = remove_skip_hyperconnection_for_sensitvity_experiment(skip_hyperconnection_config, hyperconnection_weight)
+    return (hyperconnection_weight)
 
-def remove_skip_hyperconnection_for_sensitvity_experiment(skip_hyperconnection_config, hyperconnection_weight_IoTf2, hyperconnection_weight_ef1, hyperconnection_weight_f2c):
+def remove_skip_hyperconnection_for_sensitvity_experiment(skip_hyperconnection_config, hyperconnection_weight):
     # take away the skip hyperconnection if the value in hyperconnections array is 0
-    # from IoT node to fog node 2
+    # skip_hyperconnection_config = [e1,e2,e3,e4,f3,f4,f2]
+    # from edge node 1 to fog node 2
     if skip_hyperconnection_config[0] == 0:
-        hyperconnection_weight_IoTf2 = 0
-    # from edge node to fog node 1
+        hyperconnection_weight["e1f2"] = 0
+    #from edge node 2 to fog node 2
     if skip_hyperconnection_config[1] == 0:
-        hyperconnection_weight_ef1 = 0
-    # from fog node 2 to cloud node
+        hyperconnection_weight["e2f2"] = 0
+    # from edge node 3 to fog node 2
     if skip_hyperconnection_config[2] == 0:
-        hyperconnection_weight_f2c = 0
-    return hyperconnection_weight_IoTf2, hyperconnection_weight_ef1, hyperconnection_weight_f2c
+        hyperconnection_weight["e3f2"] = 0
+    # from edge node 4 to fog node 2
+    if skip_hyperconnection_config[4] == 0:
+        hyperconnection_weight["e4f2"] = 0
+    # from fog node 3 to fog node 1
+    if skip_hyperconnection_config[5] == 0:
+        hyperconnection_weight["f3f1"] = 0
+    # from fog node 4 to fog node 1
+    if skip_hyperconnection_config[6] == 0:
+        hyperconnection_weight["f4f1"] = 0
+    # from fog node 2 to cloud node
+    if skip_hyperconnection_config[6] == 0:
+        hyperconnection_weight["f2c"] = 0
+    return hyperconnection_weight
  
-def define_hyperconnection_weight_lambda_layers(hyperconnection_weight_IoTf2, hyperconnection_weight_ef2, hyperconnection_weight_ef1, hyperconnection_weight_f2f1, hyperconnection_weight_f2c, hyperconnection_weight_f1c):
+def define_hyperconnection_weight_lambda_layers(hyperconnection_weight, connection_ends):
     # define lambdas for multiplying node weights by connection weight
-    multiply_hyperconnection_weight_layer_IoTf2 = Lambda((lambda x: x * hyperconnection_weight_IoTf2), name = "hyperconnection_weight_IoTf2")
-    multiply_hyperconnection_weight_layer_ef2 = Lambda((lambda x: x * hyperconnection_weight_ef2), name = "hyperconnection_weight_ef2")
-    multiply_hyperconnection_weight_layer_ef1 = Lambda((lambda x: x * hyperconnection_weight_ef1), name = "hyperconnection_weight_ef1")
-    multiply_hyperconnection_weight_layer_f2f1 = Lambda((lambda x: x * hyperconnection_weight_f2f1), name = "hyperconnection_weight_f2f1")
-    multiply_hyperconnection_weight_layer_f2c = Lambda((lambda x: x * hyperconnection_weight_f2c), name = "hyperconnection_weight_f2c")
-    multiply_hyperconnection_weight_layer_f1c = Lambda((lambda x: x * hyperconnection_weight_f1c), name = "hyperconnection_weight_f1c")
-    return multiply_hyperconnection_weight_layer_IoTf2, multiply_hyperconnection_weight_layer_ef2, multiply_hyperconnection_weight_layer_ef1, multiply_hyperconnection_weight_layer_f2f1, multiply_hyperconnection_weight_layer_f2c, multiply_hyperconnection_weight_layer_f1c
+    multiply_hyperconnection_weight_layer = {}
+    for connection_end in connection_ends:
+        multiply_hyperconnection_weight_layer[connection_end] = Lambda((lambda x: x * hyperconnection_weight[connection_end]), name = "hyperconnection_weight_"+connection_end)
+    return multiply_hyperconnection_weight_layer
 
-
-def define_MLP_deepFogGuard_architecture_IoT(img_input, hidden_units):
-    # use a linear Dense layer to transform input into the shape needed for the network
-    iot_output = Dense(units=hidden_units,name="skip_iotfog2",activation='linear')(img_input)
-    return iot_output
 
 def define_MLP_deepFogGuard_architecture_edge(iot_output, hidden_units):
     edge_output = define_MLP_architecture_edge(iot_output, hidden_units)
