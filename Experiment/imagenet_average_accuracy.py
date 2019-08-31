3b8beacb8e8447d48b6dfbc7e40767225b6fbdb1
briannoogin <brian.qh.nguyen@gmail.com>
2019-08-31 03:30:06 -0500

    worked on imagenet and multiview

--- Experiment/imagenet_average_accuracy.py
+++ Experiment/imagenet_average_accuracy.py
@@ -1,82 +1,97 @@
 
-from keras.applications.mobilenet import MobileNet
-import keras.backend as K
-import math
-import os 
 from Experiment.cnn_Vanilla import define_vanilla_model_CNN
 from Experiment.cnn_deepFogGuard import define_deepFogGuard_CNN
 from Experiment.cnn_deepFogGuardPlus import define_deepFogGuardPlus_CNN
-from Experiment.FailureIteration import calculateExpectedAccuracy
-from Experiment.cifar_common_exp_methods import init_data, init_common_experiment_params 
-from Experiment.utility import average, get_model_weights_CNN
-from Experiment.common_exp_methods import make_output_dictionary_average_accuracy, write_n_upload, make_results_folder
+from Experiment.FailureIterationFromImageDataGenerator import calculateExpectedAccuracyFromImageGenerator
+from Experiment.utility import average, get_model_weights_MLP_camera
+from Experiment.imagenet_common_exp_methods import init_data, init_common_experiment_params
+from Experiment.common_exp_methods import write_n_upload
+from Experiment.utility import get_model_weights_CNN_imagenet
+from Experiment.common_exp_methods import convert_to_string, make_output_dictionary_average_accuracy
+import keras.backend as K
+import datetime
 import gc
+import os
 
-def define_and_train(iteration, model_name, load_model, training_data, training_labels, val_data, val_labels, batch_size, classes, input_shape, alpha, default_failout_survival_rate, train_datagen, epochs, progress_verbose, checkpoint_verbose, train_steps_per_epoch, val_steps_per_epoch):
+def define_and_train(iteration, model_name, load_model, train_datagen, val_generator, input_shape, classes, alpha, default_failout_survival_rate):
     # ResiliNet
     if model_name == "ResiliNet":
         model = define_deepFogGuardPlus_CNN(classes=classes,input_shape = input_shape,alpha = alpha,failout_survival_setting=default_failout_survival_rate)
-        model_file = "deepFogGuardPlus_cifar_average_accuracy" + str(iteration) + ".h5"
+        model_file = "deepFogGuardPlus_imagenet_average_accuracy" + str(iteration) + ".h5"
     # deepFogGuard
     if model_name == "deepFogGuard":
         model = define_deepFogGuard_CNN(classes=classes,input_shape = input_shape,alpha = alpha)
-        model_file = "deepFogGuard_cifar_average_accuracy" + str(iteration) + ".h5"
+        model_file = "deepFogGuard_imagenet_average_accuracy" + str(iteration) + ".h5"
     # Vanilla model
     if model_name == "Vanilla":
         model = define_vanilla_model_CNN(classes=classes,input_shape = input_shape,alpha = alpha)
-        model_file = "vanilla_cifar_average_accuracy" + str(iteration) + ".h5"
+        model_file = "vanilla_cifar_imagenet_accuracy" + str(iteration) + ".h5"
     
-    get_model_weights_CNN(model, model_name, load_model, model_file, training_data, training_labels, val_data, val_labels, train_datagen, batch_size, epochs, progress_verbose, checkpoint_verbose, train_steps_per_epoch, val_steps_per_epoch)
+    get_model_weights_CNN_imagenet(model, model_name, load_model, model_file, train_generator, val_generator,verbose)
     return model
 
+def calc_accuracy(iteration, model_name, model, survivability_setting, output_list,test_generator, num_test_examples):
+    output_list.append(model_name + "\n")
+    print(model_name)
+    output[model_name][str(survivability_setting)][iteration-1] = calculateExpectedAccuracyFromImageGenerator(model,survivability_setting,output_list,test_generator,num_test_examples = num_test_examples)
+
+
+# runs all 3 failure configurations for all 3 models
 if __name__ == "__main__":
-    training_data, test_data, training_labels, test_labels, val_data, val_labels = init_data() 
+    use_GCP = False
+    train_generator, test_generator = init_data(use_GCP) 
+    num_train_examples,num_test_examples, survivability_settings, input_shape, num_classes, alpha = init_common_experiment_params()
 
-    num_iterations, classes, survivability_settings, train_datagen, batch_size, epochs, progress_verbose, checkpoint_verbose, use_GCP, alpha, input_shape = init_common_experiment_params()
-    
-    default_failout_survival_rate = [.95,.95]
-    train_steps_per_epoch = math.ceil(len(training_data) / batch_size)
-    val_steps_per_epoch = math.ceil(len(val_data) / batch_size)
-
-    output = make_output_dictionary_average_accuracy(survivability_settings, num_iterations)
+    default_failout_survival_rate = [.95,.95,.95]
+    allpresent_skip_hyperconnections_configuration = [1,1,1]
+    default_survivability_setting = [1,1,1]
     load_model = False
-    
-    make_results_folder()
-    file_name = 'results' + '/cifar_average_accuracy_results.txt'
+
+    # file name with the experiments accuracy output
+    output_name = "results/imagenet_average_accuracy.txt"
+    num_iterations = 1
+    verbose = 2
+
+    # keep track of output so that output is in order
     output_list = []
-    for iteration in range(1,num_iterations+1):
-        print("iteration:",iteration)
-        Vanilla = define_and_train(iteration, "Vanilla", load_model, training_data, training_labels, val_data, val_labels, batch_size, classes, input_shape, alpha, default_failout_survival_rate, train_datagen, epochs, progress_verbose, checkpoint_verbose, train_steps_per_epoch, val_steps_per_epoch)
-        deepFogGuard = define_and_train(iteration,"deepFogGuard", load_model, training_data, training_labels, val_data, val_labels, batch_size, classes, input_shape, alpha, default_failout_survival_rate, train_datagen, epochs, progress_verbose, checkpoint_verbose, train_steps_per_epoch, val_steps_per_epoch)
-        ResiliNet = define_and_train(iteration, "ResiliNet", load_model, training_data, training_labels, val_data, val_labels, batch_size, classes, input_shape, alpha, default_failout_survival_rate, train_datagen, epochs, progress_verbose, checkpoint_verbose, train_steps_per_epoch, val_steps_per_epoch)
-        
+    
+    output = make_output_dictionary_average_accuracy(survivability_settings, num_iterations)
+
+    # make folder for outputs 
+    if not os.path.exists('results/'):
+        os.mkdir('results/')
+    for iteration in range(1,num_iterations+1):   
+        output_list.append('ITERATION ' + str(iteration) +  '\n')
+        print("ITERATION ", iteration)
+        ResiliNet = define_and_train(iteration, "ResiliNet", load_model, train_generator, val_generator, input_shape, num_classes, alpha, default_failout_survival_rate)
+        deepFogGuard = define_and_train(iteration, "deepFogGuard", load_model, train_generator, val_generator, input_shape, num_classes, alpha, default_failout_survival_rate)
+        Vanilla = define_and_train(iteration, "Vanilla", load_model, train_generator,val_generator, input_shape, num_classes,alpha,default_failout_survival_rate)
+ 
+        # test models
         for survivability_setting in survivability_settings:
-            output_list.append(str(survivability_setting) + '\n')
-            print(survivability_setting)
-            output["Vanilla"][str(survivability_setting)][iteration-1] = calculateExpectedAccuracy(Vanilla, survivability_setting,output_list, training_labels, test_data, test_labels)
-            output["deepFogGuard"][str(survivability_setting)][iteration-1] = calculateExpectedAccuracy(deepFogGuard, survivability_setting,output_list, training_labels, test_data, test_labels)
-            output["ResiliNet"][str(survivability_setting)][iteration-1] = calculateExpectedAccuracy(ResiliNet, survivability_setting,output_list, training_labels, test_data, test_labels)
+            calc_accuracy(iteration, "ResiliNet", ResiliNet, survivability_setting, output_list,test_generator, num_test_examples)
+            calc_accuracy(iteration, "deepFogGuard", deepFogGuard, survivability_setting, output_list,test_generator, num_test_examples)
+            calc_accuracy(iteration, "Vanilla", Vanilla, survivability_setting, output_list,test_generator, num_test_examples)
+            
         # clear session so that model will recycled back into memory
         K.clear_session()
         gc.collect()
-        del Vanilla
         del deepFogGuard
         del ResiliNet
-   
+        del Vanilla
+   # calculate average accuracies from all expected accuracies
     for survivability_setting in survivability_settings:
-        output_list.append(str(survivability_setting) + '\n')
-
-        Vanilla_acc = average(output["Vanilla"][str(survivability_setting)])
-        deepFogGuard_acc = average(output["deepFogGuard"][str(survivability_setting)])
         ResiliNet_acc = average(output["ResiliNet"][str(survivability_setting)])
+        deepFogGuard_acc = average(output["deepFogGuard"][str(survivability_setting)])
+        Vanilla_acc = average(output["Vanilla"][str(survivability_setting)])
 
-        output_list.append(str(survivability_setting) + " Vanilla Accuracy: " + str(Vanilla_acc) + '\n')
-        output_list.append(str(survivability_setting) + " deepFogGuard Accuracy: " + str(deepFogGuard_acc) + '\n')
         output_list.append(str(survivability_setting) + " ResiliNet Accuracy: " + str(ResiliNet_acc) + '\n')
+        output_list.append(str(survivability_setting) + " deepFogGuard Accuracy: " + str(deepFogGuard_acc) + '\n')
+        output_list.append(str(survivability_setting) + " Vanilla Accuracy: " + str(Vanilla_acc) + '\n')
 
-        print(str(survivability_setting),"Vanilla Accuracy:",Vanilla_acc)
-        print(str(survivability_setting),"deepFogGuard Accuracy:",deepFogGuard_acc)
         print(str(survivability_setting),"ResiliNet Accuracy:",ResiliNet_acc)
-    
+        print(str(survivability_setting),"deepFogGuard Accuracy:",deepFogGuard_acc)
+        print(str(survivability_setting),"Vanilla Accuracy:",Vanilla_acc)
+
     write_n_upload(output_name, output_list, use_GCP)
     print(output)
--- Experiment/multiview_average_accuracy.py
+++ Experiment/multiview_average_accuracy.py
@@ -1,8 +1,8 @@
 
-from Experiment.mlp_deepFogGuardPlus_health import define_deepFogGuardPlus_MLP
-from Experiment.mlp_deepFogGuard_health import define_deepFogGuard_MLP
-from Experiment.mlp_Vanilla_health import define_vanilla_model_MLP
-from Experiment.FailureIteration import calculateExpectedAccuracy
+from Experiment.mlp_deepFogGuardPlus_camera import define_deepFogGuardPlus_MLP
+from Experiment.mlp_deepFogGuard_camera import define_deepFogGuard_MLP
+from Experiment.mlp_Vanilla_camera import define_vanilla_model_MLP
+from Experiment.FailureIterationFromImageDataGenerator import calculateExpectedAccuracyFromImageGenerator
 from Experiment.utility import average, get_model_weights_MLP_camera
 from Experiment.multiview_common_exp_methods import init_data, init_common_experiment_params
 from Experiment.common_exp_methods import write_n_upload
@@ -13,34 +13,34 @@ import datetime
 import gc
 import os
 
-def define_and_train(iteration, model_name, load_model,train_generator, val_generator, num_classes, hidden_units, verbose, default_failout_survival_rate, default_survivability_setting, allpresent_skip_hyperconnections_configuration, datagen):
+def define_and_train(iteration, model_name, load_model,train_generator, val_generator, input_shape, num_classes, hidden_units, verbose, default_failout_survival_rate, default_survivability_setting, allpresent_skip_hyperconnections_configuration):
     # ResiliNet
     if model_name == "ResiliNet":
-        model = define_deepFogGuardPlus_MLP(num_vars,num_classes,hidden_units,default_failout_survival_rate)
+        model = define_deepFogGuardPlus_MLP(input_shape,num_classes,hidden_units,default_failout_survival_rate)
         model_file = "new_split_" + str(iteration) + '_deepFogGuardPlus.h5'
     # deepFogGuard
     if model_name == "deepFogGuard":
-        model = define_deepFogGuard_MLP(num_vars, num_classes, hidden_units, default_survivability_setting, allpresent_skip_hyperconnections_configuration)
+        model = define_deepFogGuard_MLP(input_shape, num_classes, hidden_units, default_survivability_setting, allpresent_skip_hyperconnections_configuration)
         model_file = "new_split_" + str(iteration) + '_deepFogGuard.h5'
     # Vanilla model
     if model_name == "Vanilla":
-        model = define_vanilla_model_MLP(num_vars,num_classes,hidden_units)
+        model = define_vanilla_model_MLP(input_shape,num_classes,hidden_units)
         model_file = "new_split_" + str(iteration) + '_vanilla.h5'
     
     get_model_weights_MLP_camera(model, model_name, load_model, model_file, train_generator, val_generator, verbose)
     return model
 
-def calc_accuracy(iteration, model_name, model, survivability_setting, output_list,training_labels,test_data,test_labels):
+def calc_accuracy(iteration, model_name, model, survivability_setting, output_list,test_generator, num_test_examples):
     output_list.append(model_name + "\n")
     print(model_name)
-    output[model_name][str(survivability_setting)][iteration-1] = calculateExpectedAccuracy(model,survivability_setting,output_list,training_labels,test_data,test_labels)
+    output[model_name][str(survivability_setting)][iteration-1] = calculateExpectedAccuracyFromImageGenerator(model,survivability_setting,output_list,test_generator,num_test_examples = num_test_examples)
 
 
 # runs all 3 failure configurations for all 3 models
 if __name__ == "__main__":
     use_GCP = True
     train_generator, val_generator, test_generator = init_data(use_GCP) 
-    num_iterations, num_vars, num_classes, survivability_settings, num_train_epochs, hidden_units, batch_size = init_common_experiment_params(training_data)
+    num_train_examples,num_val_examples,num_test_examples, survivability_settings, input_shape, num_classes, hidden_units = init_common_experiment_params()
 
     default_failout_survival_rate = [.95,.95,.95]
     allpresent_skip_hyperconnections_configuration = [1,1,1]
@@ -63,15 +63,15 @@ if __name__ == "__main__":
     for iteration in range(1,num_iterations+1):   
         output_list.append('ITERATION ' + str(iteration) +  '\n')
         print("ITERATION ", iteration)
-        ResiliNet = define_and_train(iteration, "ResiliNet", load_model, training_data, training_labels, val_data, val_labels, num_train_epochs, batch_size, num_vars, num_classes, hidden_units, verbose, default_failout_survival_rate, default_survivability_setting, allpresent_skip_hyperconnections_configuration)
-        deepFogGuard = define_and_train(iteration, "deepFogGuard", load_model, training_data, training_labels, val_data, val_labels, num_train_epochs, batch_size, num_vars, num_classes, hidden_units, verbose, default_failout_survival_rate, default_survivability_setting, allpresent_skip_hyperconnections_configuration)
-        Vanilla = define_and_train(iteration, "Vanilla", load_model, training_data, training_labels, val_data, val_labels, num_train_epochs, batch_size, num_vars, num_classes, hidden_units, verbose, default_failout_survival_rate, default_survivability_setting, allpresent_skip_hyperconnections_configuration)
+        ResiliNet = define_and_train(iteration, "ResiliNet", load_model, train_generator, val_generator, input_shape, num_classes, hidden_units, verbose, default_failout_survival_rate, default_survivability_setting, allpresent_skip_hyperconnections_configuration)
+        deepFogGuard = define_and_train(iteration, "deepFogGuard", load_model, train_generator, val_generator, input_shape, num_classes, hidden_units, verbose, default_failout_survival_rate, default_survivability_setting, allpresent_skip_hyperconnections_configuration)
+        Vanilla = define_and_train(iteration, "Vanilla", load_model, train_generator,val_generator, input_shape, num_classes, hidden_units, verbose, default_failout_survival_rate, default_survivability_setting, allpresent_skip_hyperconnections_configuration)
  
         # test models
         for survivability_setting in survivability_settings:
-            calc_accuracy(iteration, "ResiliNet", ResiliNet, survivability_setting, output_list,training_labels,test_data,test_labels)
-            calc_accuracy(iteration, "deepFogGuard", deepFogGuard, survivability_setting, output_list,training_labels,test_data,test_labels)
-            calc_accuracy(iteration, "Vanilla", Vanilla, survivability_setting, output_list,training_labels,test_data,test_labels)
+            calc_accuracy(iteration, "ResiliNet", ResiliNet, survivability_setting, output_list,test_generator, num_test_examples)
+            calc_accuracy(iteration, "deepFogGuard", deepFogGuard, survivability_setting, output_list,test_generator, num_test_examples)
+            calc_accuracy(iteration, "Vanilla", Vanilla, survivability_setting, output_list,test_generator, num_test_examples)
             
         # clear session so that model will recycled back into memory
         K.clear_session()
