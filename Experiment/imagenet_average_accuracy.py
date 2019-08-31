f0c24b9e7039d7818ca239fe514c9ef673f3217b
briannoogin <brian.qh.nguyen@gmail.com>
2019-08-31 17:39:50 -0500

    fixed imports and directory name for imagenet dataset

--- Experiment/imagenet_average_accuracy.py
+++ Experiment/imagenet_average_accuracy.py
@@ -3,9 +3,8 @@ from Experiment.common_exp_methods_CNN import define_model
 from Experiment.FailureIterationFromImageDataGenerator import calculateExpectedAccuracyFromImageGenerator
 from Experiment.utility import average, get_model_weights_MLP_camera
 from Experiment.imagenet_common_exp_methods import init_data, init_common_experiment_params
-from Experiment.common_exp_methods import write_n_upload
 from Experiment.utility import get_model_weights_CNN_imagenet
-from Experiment.common_exp_methods import convert_to_string, make_output_dictionary_average_accuracy, make_results_folder
+from Experiment.common_exp_methods import convert_to_string, make_output_dictionary_average_accuracy, make_results_folder,write_n_upload
 import keras.backend as K
 import datetime
 import gc
