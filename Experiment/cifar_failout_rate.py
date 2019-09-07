e2b3a47d663446c7a53cde2df8f853bdfca59855
briannoogin <brian.qh.nguyen@gmail.com>
2019-09-07 14:44:59 -0500

    fix parameter naming

--- Experiment/cifar_failout_rate.py
+++ Experiment/cifar_failout_rate.py
@@ -15,7 +15,7 @@ from Experiment.utility import average, get_model_weights_CNN
 import gc
 
 
-def define_and_train(iteration, model_name, load_model, survivability_setting, training_data, training_labels, val_data, val_labels, batch_size, classes, input_shape, alpha, strides, train_datagen, epochs, progress_verbose, checkpoint_verbose, train_steps_per_epoch, val_steps_per_epoch):
+def define_and_train(iteration, model_name, load_model, failout_survival_setting, training_data, training_labels, val_data, val_labels, batch_size, classes, input_shape, alpha, strides, train_datagen, epochs, progress_verbose, checkpoint_verbose, train_steps_per_epoch, val_steps_per_epoch):
     model_file = str(iteration) + " " + str(failout_survival_setting) + 'cifar_failout_rate.h5'
     model = define_deepFogGuardPlus_CNN(classes=classes,input_shape = input_shape,alpha = alpha,failout_survival_setting=survivability_setting, strides = strides)
     get_model_weights_CNN(model, model_name, load_model, model_file, training_data, training_labels, val_data, val_labels, train_datagen, batch_size, epochs, progress_verbose, checkpoint_verbose, train_steps_per_epoch, val_steps_per_epoch)
